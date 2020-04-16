e129b983e1200456484dbff088475540c8f1ba57
Philip Whiteside <philipwhiteside@users.noreply.github.com>
2020-04-16 21:05:12 +0100

    Move tenant name to alias & standardise tenant dn with TF resource name

--- examples/exercise1/config.tf
+++ examples/exercise1/config.tf
@@ -10,7 +10,8 @@ provider "aci" {
 }
 
 resource "aci_tenant" "terraform_tenant" {
-  name        = "tenant_for_terraform"
+  name        = "terraform_tenant"
+  name_alias  = "tenant_for_terraform"
   description = "This tenant is created by terraform ACI provider"
 }
 
