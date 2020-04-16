32076548279b6ef043f299f17523d819639fc31d
Philip Whiteside <philipwhiteside@users.noreply.github.com>
2020-04-16 21:16:06 +0100

    Add VRF and associate BD

--- examples/exercise1/config.tf
+++ examples/exercise1/config.tf
@@ -26,11 +26,18 @@ resource "aci_tenant" "terraform_tenant" {
 }
 
 # Networking Definition
+resource "aci_vrf" "default" {
+  tenant_dn              = "${aci_tenant.terraform_tenant.id}"
+  name                   = "default"
+  name_alias             = "default"
+}
+
 resource "aci_bridge_domain" "bd_for_subnet" {
   tenant_dn   = "${aci_tenant.terraform_tenant.id}"
   name        = "bd_for_subnet"
   description = "This bridge domain is created by terraform ACI provider"
   mac         = "00:22:BD:F8:19:FF"
+  relation_fv_rs_ctx = "${aci_vrf.default.name}"
 }
 
 resource "aci_subnet" "demosubnet" {
